9620aacdd01213406a48901f33b39e0945628e77
Takahiro Harada <takahiroharada@gmail.com>
2014-05-17 21:08:40 -0700

    UnitTests.

---
+++ UnitTest/main.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+
+
+TEST(UnitTest, Test1)
+{
+
+}
+
+TEST(UnitTest, Test2)
+{
+	
+}
+
+int main(int argc, char* argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+	return RUN_ALL_TESTS();
+}
---
+++ premake4.lua
@@ -0,0 +1,42 @@
+solution "TravisCITest"
+	configurations { "Debug", "Release" }
+	configuration "Debug"
+		defines { "_DEBUG" }
+		flags { "Symbols" }
+	configuration "Release"
+		defines { "NDEBUG" }
+		flags { "Optimize" }
+	configuration {}
+	platforms { "x64" }
+			
+	language "C++"
+	flags { "NoMinimalRebuild", "EnableSSE", "EnableSSE2" }
+		
+	configuration {"x64", "Debug"}
+		targetsuffix "64D"
+	configuration {"x64", "Release"}
+		targetsuffix "64"
+	configuration {}
+
+	includedirs{ "./include" }
+	
+	configuration {}
+	project "UnitTest"
+		kind "ConsoleApp"
+		location "../UnitTest"
+		targetdir "./build"
+		
+		files { "./UnitTest/*.cpp", "./gtest-1.6.0/**" } 
+			
+		if _ACTION == "vs2012" then
+			defines{ "GTEST_HAS_TR1_TUPLE=0" }
+		end
+		
+		if os.is("macosx") then
+			buildoptions{"-DGTEST_USE_OWN_TR1_TUPLE=1"}
+		end
+
+		configuration {}
+				
+
+
